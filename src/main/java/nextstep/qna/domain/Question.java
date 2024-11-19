a35d605671ae03303b5d52c3ae2c51aebd55104e
ujoonnee <dnwns0933@naver.com>
2024-11-19 17:49:57 +0900

    feat : 질문 작성자가 본인인지 확인하는 메서드 추가

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -70,6 +71,16 @@ public class Question {
 
     public boolean isOwner(NsUser loginUser) {
         return writer.equals(loginUser);
+    }
+
+    public void checkAuthority(NsUser user) throws CannotDeleteException {
+        checkOwner(user);
+    }
+
+    private void checkOwner(NsUser user) throws CannotDeleteException {
+        if (!isOwner(user)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
     }
 
     public Question setDeleted(boolean deleted) {
